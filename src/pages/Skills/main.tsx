5eb99bc38944048a29afc1d3ae3a25d9dfcf0f15
User-Felix <sebastian.felix.coder@proton.me>
2025-02-01 00:56:15 +0000

    create page skills

--- src/pages/Skills/main.tsx
+++ src/pages/Skills/main.tsx
@@ -1,13 +1,26 @@
 import Toolbar from "../../components/toollbar/toolbar";
+import { Link } from "react-router-dom";
+
+import './skills.css'
 
 export default function Skills(): React.ReactElement {
     return(
         <>
             <Toolbar/>
             <main>
-                <p>
-                    Skills construindo
-                </p>
+                <div id="card-vercel">
+                    <div id="button-div">
+                        <Link target="_blank" rel="noopener noreferrer" to="https://github.com/Raposa-Dev">
+                            My Github
+                        </Link>
+                    </div>
+                    
+                    <img height = "220em" src = "https://github-readme-stats.vercel.app/api?username=User-Felix&show_icons=true&theme=synthwave&include_all_commits=true&count_private=true" />
+                    <div id='content-skills'>
+                        <img height = "220em" src = "https://github-readme-stats.vercel.app/api/top-langs/?username=User-Felix&theme=synthwave" />
+                        <img height = "220em" src = "https://github-readme-stats.vercel.app/api/top-langs/?username=Raposa-Dev&theme=synthwave" />
+                    </div>
+                </div>
             </main>
         </>
     )
---
+++ src/pages/works/main.tsx
@@ -0,0 +1,12 @@
+import Toolbar from "../../components/toollbar/toolbar"
+
+export default function Works() {
+    return(
+        <>
+            <Toolbar/>
+            <main>
+                <p>Meus trabalhos</p>
+            </main>
+        </>
+    )
+}
