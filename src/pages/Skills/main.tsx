b7a6536f0bea85c49dfd830abf3b919428c0a9d1
Felix <sebastian.felix.coder@proton.me>
2025-03-02 20:48:15 +0000

    update css in skills

--- src/pages/Skills/main.tsx
+++ src/pages/Skills/main.tsx
@@ -7,19 +7,20 @@ export default function Skills(): React.ReactElement {
     return(
         <>
             <Toolbar/>
-            <main>
+            <main id="container-skills">
                 <div id="card-vercel">
                     <div id="button-div">
                         <Link target="_blank" rel="noopener noreferrer" to="https://github.com/Raposa-Dev">
                             My Github
                         </Link>
                     </div>
-                    
-                    <img height = "220em" src = "https://github-readme-stats.vercel.app/api?username=User-Felix&show_icons=true&theme=synthwave&include_all_commits=true&count_private=true" />
-                    <div id='content-skills'>
-                        <img height = "220em" src = "https://github-readme-stats.vercel.app/api/top-langs/?username=User-Felix&theme=synthwave" />
-                        <img height = "220em" src = "https://github-readme-stats.vercel.app/api/top-langs/?username=Raposa-Dev&theme=synthwave" />
-                    </div>
+                    <div id="separate">
+                        <img height = "220em" src = "https://github-readme-stats.vercel.app/api?username=User-Felix&show_icons=true&theme=synthwave&include_all_commits=true&count_private=true" />
+                        <div id='content-skills'>
+                            <img height = "220em" src = "https://github-readme-stats.vercel.app/api/top-langs/?username=User-Felix&theme=synthwave" />
+                            <img height = "220em" src = "https://github-readme-stats.vercel.app/api/top-langs/?username=Raposa-Dev&theme=synthwave" />
+                        </div>
+                    </div>   
                 </div>
             </main>
         </>
